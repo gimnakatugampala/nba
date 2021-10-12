b2d916611a4a27f2f6c21d9794f4acf8c97f42ad
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-10-12 07:23:04 -0700

    navbar icons

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View  } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default function Navbar() {
     return (
         <View style={styles.navbarMain}>
-            <Text style={styles.navbarText}>Home</Text>
-            <Text style={styles.navbarText}>Staning</Text>
-            <Text style={styles.navbarText}>Player</Text>
-            <Text style={styles.navbarText}>Team</Text>
-            <Text style={styles.navbarText}>State</Text>
+            <View style={styles.navbarText}>
+            <Icon name="home" size={15} color="#fff" style={styles.icons} />
+            <Text style={styles.navbarInside}>Home</Text>
+        </View>
+
+            <View style={styles.navbarText}>
+            <Icon name="bar-chart-o" size={15} color="#fff" style={styles.icons} />
+            <Text style={styles.navbarInside}>Standing</Text>
+        </View>
+
+        <View style={styles.navbarText}>
+            <Icon name="user" size={15} color="#fff" style={styles.icons} />
+            <Text style={styles.navbarInside}>Player</Text>
+        </View>
+
+        <View style={styles.navbarText}>
+            <Icon name="users" size={15} color="#fff" style={styles.icons} />
+            <Text style={styles.navbarInside}>Team</Text>
+        </View>
+
+        <View style={styles.navbarText}>
+            <Icon name="area-chart" size={15} color="#fff" style={styles.icons} />
+            <Text style={styles.navbarInside}>Stats</Text>
+        </View>
+
         </View>
     )
 }
@@ -25,9 +47,15 @@ const styles = StyleSheet.create({
         
     },
     navbarText:{
-        color:'#fff',
         flex:1,
-        padding:5,
+        justifyContent:'center'
+    },
+    navbarInside:{
+        color:'#fff',
+        textAlign:'center'
+
+    },
+    icons:{
         textAlign:'center'
     }
 })
