369ac4a18a503c8a9f47070adfe9e23d8ad21d2a
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-10-13 15:12:21 -0700

    players ui

--- screens/Players.js
+++ screens/Players.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { StyleSheet, Text, View ,StatusBar } from 'react-native'
 
+import Header from '../components/Header'
+
 export default function Players() {
     return (
         <View>
              <StatusBar barStyle="light-content" backgroundColor="#0054a4" />
+
+            {/* Header */}
+            <Header />
+
 
             <Text>Player</Text>
         </View>
